9840f11b6ba835407656082411c553329039c02d
Pawan Awasthi <pawanawasthi481@gmail.com>
2024-02-18 14:03:25 +0545

    feat: clerk authentication initiated

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,18 @@
+import MobileNav from '@/components/shared/MobileNav';
+import Sidebar from '@/components/shared/Sidebar';
+import { ReactNode } from 'react';
+
+const Layout = ({ children }: { children: ReactNode }) => {
+  return (
+    <main className='root'>
+      <Sidebar />
+      <MobileNav />
+
+      <div className='root-container'>
+        <div className='wrapper'>{children}</div>
+      </div>
+    </main>
+  );
+};
+
+export default Layout;
